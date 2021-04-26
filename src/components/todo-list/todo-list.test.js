70fa85b7de278d9b8c9596ee352000c0d82f3aa7
Ibrahim <ibrahim.alrayany@gmail.com>
2021-04-26 06:22:26 +0200

    add group of test in case of empty todo list

--- src/components/todo-list/todo-list.test.js
+++ src/components/todo-list/todo-list.test.js
@@ -30,11 +30,21 @@ test('render title without error', () => {
   expect(title.text().length).not.toBe(0);
 })
 
-test('show "No tasks message" when list is empty', () => {
-  const wrapper = setup();
-  const emptyMsg = findByTestAttr(wrapper, 'empty-message');
-  expect(emptyMsg.exists()).toBeTruthy();
-});
+describe('if there is no todo in the list', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup();
+  });
+  test('renders without error', () => {
+    const app = findByTestAttr(wrapper, 'component-todo-list');
+    expect(app.exists()).toBeTruthy();
+  });
+  test('show "No tasks message" when list is empty', () => {
+    const emptyMsg = findByTestAttr(wrapper, 'empty-message');
+    expect(emptyMsg.exists()).toBeTruthy();
+  });
+})
+
 
 test('show list element when list is not empty', () => {
   const wrapper = setup({ todoList: [{ value: "value-test", id: 'test-id' }] });
