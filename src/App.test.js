2a2f9e86ee05930841106d152c772a05c9c368e3
Ibrahim <ibrahim.alrayany@gmail.com>
2021-05-31 08:10:33 +0200

    Refactor the App.test.js file

--- src/App.test.js
+++ src/App.test.js
@@ -16,38 +16,21 @@ jest.mock('./actions');
 import { getTodo as mockGetTodo } from './actions'
 
 const setup = (state = {}) => {
-  // TODO: apply state 
   const wrapper = mount(<App />);
-  // add value to input box 
-  const inputBox = findByTestAttr(wrapper, 'input-form');
-  inputBox.simulate('change', { target: { value: 'train' } });
-  // simulate click on submit button      
-  const submitButton = findByTestAttr(wrapper, 'button-form');
-  submitButton.simulate('click', { preventDefault() { } });
   return wrapper;
 }
 
-describe('no words in the list', () => {
-  test('testList has length of ONE', () => {
-    let wrapper = setup();
-    const testList = findByTestAttr(wrapper, 'todo-item');
-    expect(testList).toHaveLength(1);
+test('render without error', () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
+  expect(appComponent).toHaveLength(1);
+})
+
+describe('ge a todo', () => {
+  test('getTodo on app mount', () => {
+
   });
-});
+  test('getTodo does NOT run on update', () => {
 
-describe('ONE word on the list', () => {
-  test('testList has length of TWO', () => {
-    let wrapper = setup();
-    // add value to input box 
-    const inputBox = findByTestAttr(wrapper, 'input-form');
-    inputBox.simulate('change', { target: { value: 'train' } });
-    // simulate click on submit button      
-    const submitButton = findByTestAttr(wrapper, 'button-form');
-    submitButton.simulate('click', { preventDefault() { } });
-
-    // So now the list has two elements, one that added on the setup() 
-    // method and the other that added here.
-    const testList = findByTestAttr(wrapper, 'todo-item');
-    expect(testList).toHaveLength(2);
   });
 });
