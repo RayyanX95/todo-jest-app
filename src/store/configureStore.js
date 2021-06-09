4e518152cc0a5f857852139eb1f46836c3635248
Ibrahim <ibrahim.alrayany@gmail.com>
2021-06-09 06:56:55 +0200

    Add redux-thunk to the project

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+
+export const middlewareList = [ReduxThunk];
 
 import rootReducer from './reducers';
 
-export default createStore(rootReducer);
+export default createStore(rootReducer, {}, applyMiddleware(...middlewareList));;
--- test/testUtils.js
+++ test/testUtils.js
@@ -1,9 +1,10 @@
 import checkPropTypes from 'check-prop-types';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import rootReducer from '../src/store/reducers/index';
+import { middlewareList } from '../src/store/configureStore';
 
-export const storeFactory = (initialState) => createStore(rootReducer, initialState);
+export const storeFactory = (initialState) => createStore(rootReducer, initialState, applyMiddleware(...middlewareList));
 
 export const findByTestAttr = (wrapper, value) => wrapper.find(`[data-test='${value}']`);
 
